133b45b6b30f30a659f943fc66e5075eee6dbe11
williamzabot <williamzabot123@gmail.com>
2024-02-12 12:27:55 -0300

    add divider

--- app/src/main/java/com/williamzabolt/iptest/MainActivity.kt
+++ app/src/main/java/com/williamzabolt/iptest/MainActivity.kt
@@ -11,10 +11,12 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -112,6 +114,13 @@ class MainActivity : ComponentActivity() {
                         fontSize = 16.sp,
                     )
                 }
+
+                Divider(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(1.dp)
+
+                )
             }
         }
     }
